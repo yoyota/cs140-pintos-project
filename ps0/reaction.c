6367ea68cabd08abd68ef771de2a67c25fd77c1f
huhuta <smarthorse6@gmail.com>
2018-12-15 23:17:03 +0900

    implement reaction.c

--- ps0/reaction.c
+++ ps0/reaction.c
@@ -6,23 +6,35 @@
 void make_water();
 
 struct reaction {
-	// FILL ME IN
+  int h_atom_count;
+  struct condition h2o_reaction;
+  struct condition h_atom_creation;
+  struct lock lock;
 };
 
-void
-reaction_init(struct reaction *reaction)
-{
-	// FILL ME IN
+void reaction_init(struct reaction *reaction) {
+  reaction->h_atom_count = 0;
+  cond_init(&reaction->h2o_reaction);
+  cond_init(&reaction->h_atom_creation);
+  lock_init(&reaction->lock);
 }
 
-void
-reaction_h(struct reaction *reaction)
-{
-	// FILL ME IN
+void reaction_h(struct reaction *reaction) {
+  lock_acquire(&reaction->lock);
+  reaction->h_atom_count += 1;
+  cond_signal(&reaction->h_atom_creation, &reaction->lock);
+  cond_wait(&reaction->h2o_reaction, &reaction->lock);
+  lock_release(&reaction->lock);
 }
 
-void
-reaction_o(struct reaction *reaction)
-{
-	// FILL ME IN
+void reaction_o(struct reaction *reaction) {
+  lock_acquire(&reaction->lock);
+  while (reaction->h_atom_count < 2) {
+    cond_wait(&reaction->h_atom_creation, &reaction->lock);
+  }
+  reaction->h_atom_count -= 2;
+  make_water();
+  cond_signal(&reaction->h2o_reaction, &reaction->lock);
+  cond_signal(&reaction->h2o_reaction, &reaction->lock);
+  lock_release(&reaction->lock);
 }
