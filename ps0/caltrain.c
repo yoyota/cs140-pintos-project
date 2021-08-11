6ce43903f5c21eb2412b25f5a821591ac2bd4ff2
YongTak Yoo <yongtaky@gmail.com>
2021-08-11 13:50:35 +0900

    Caltrain with fewer lines of code

--- ps0/caltrain.c
+++ ps0/caltrain.c
@@ -1,56 +1,42 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <unistd.h>
 #include "pintos_thread.h"
 
 struct station {
-  int passengers_wait_for_train;
-  int available_seats;
-  int passengers_can_board;
   struct lock lock;
-  struct condition train_arrival;
-  struct condition train_departure;
+  struct condition cond;
+  int seats;
+  int passengers;
 };
 
 void station_init(struct station *station) {
-  station->passengers_wait_for_train = 0;
-  station->available_seats = 0;
-  station->passengers_can_board = 0;
+  station->seats = 0;
+  station->passengers = 0;
   lock_init(&station->lock);
-  cond_init(&station->train_arrival);
-  cond_init(&station->train_departure);
+  cond_init(&station->cond);
 }
 
 void station_load_train(struct station *station, int count) {
   lock_acquire(&station->lock);
-  if (count == 0 || station->passengers_wait_for_train == 0) {
-    lock_release(&station->lock);
-    return;
+  station->seats = count;
+  for (int i = 0; i < count; i += 1) {
+    cond_signal(&station->cond, &station->lock);
   }
-  station->available_seats = count;
-  station->passengers_can_board = count;
-  cond_broadcast(&station->train_arrival, &station->lock);
-  cond_wait(&station->train_departure, &station->lock);
+  while (station->passengers > 0 && station->seats > 0) {
+    lock_release(&station->lock);
+    lock_acquire(&station->lock);
+  }
   lock_release(&station->lock);
 }
 
 void station_wait_for_train(struct station *station) {
   lock_acquire(&station->lock);
-  station->passengers_wait_for_train += 1;
-  while (station->available_seats == 0) {
-    cond_wait(&station->train_arrival, &station->lock);
-  }
-  station->available_seats -= 1;
+  station->passengers += 1;
+  cond_wait(&station->cond, &station->lock);
   lock_release(&station->lock);
 }
 
 void station_on_board(struct station *station) {
   lock_acquire(&station->lock);
-  station->passengers_can_board -= 1;
-  station->passengers_wait_for_train -= 1;
-  if (station->passengers_can_board == 0 ||
-      station->passengers_wait_for_train == 0) {
-    cond_signal(&station->train_departure, &station->lock);
-  }
+  station->seats -= 1;
+  station->passengers -= 1;
   lock_release(&station->lock);
 }
