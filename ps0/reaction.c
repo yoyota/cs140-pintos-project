9ba1d82677569f77e616c82b556ac6b4133b0a5f
YongTak Yoo <yongtaky@gmail.com>
2021-08-12 23:18:08 +0900

    Predicate testing loop

--- ps0/reaction.c
+++ ps0/reaction.c
@@ -6,38 +6,42 @@
 void make_water();
 
 struct reaction {
-	int h_atom_count;
-	struct condition h2o_reaction;
-	struct condition h_atom_creation;
+	int h_count;
+	int h_reacted;
 	struct lock lock;
+	struct condition cond;
 };
 
 void reaction_init(struct reaction *reaction)
 {
-	reaction->h_atom_count = 0;
-	cond_init(&reaction->h2o_reaction);
-	cond_init(&reaction->h_atom_creation);
+	reaction->h_count = 0;
+	reaction->h_reacted = 0;
 	lock_init(&reaction->lock);
+	cond_init(&reaction->cond);
 }
 
 void reaction_h(struct reaction *reaction)
 {
 	lock_acquire(&reaction->lock);
-	reaction->h_atom_count += 1;
-	cond_signal(&reaction->h_atom_creation, &reaction->lock);
-	cond_wait(&reaction->h2o_reaction, &reaction->lock);
+	reaction->h_count += 1;
+	cond_broadcast(&reaction->cond, &reaction->lock);
+
+	while (reaction->h_reacted == 0) {
+		cond_wait(&reaction->cond, &reaction->lock);
+	}
+	reaction->h_reacted -= 1;
 	lock_release(&reaction->lock);
 }
 
 void reaction_o(struct reaction *reaction)
 {
 	lock_acquire(&reaction->lock);
-	while (reaction->h_atom_count < 2) {
-		cond_wait(&reaction->h_atom_creation, &reaction->lock);
+	while (reaction->h_count < 2) {
+		cond_wait(&reaction->cond, &reaction->lock);
 	}
-	reaction->h_atom_count -= 2;
-	make_water();
-	cond_signal(&reaction->h2o_reaction, &reaction->lock);
-	cond_signal(&reaction->h2o_reaction, &reaction->lock);
+	reaction->h_count -= 2;
+	reaction->h_reacted += 2;
+	cond_broadcast(&reaction->cond, &reaction->lock);
 	lock_release(&reaction->lock);
+	make_water();
 }
