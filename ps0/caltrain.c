f797ff8e47160e970f778e84dbcac1eb69bd3856
YongTak Yoo <yongtaky@gmail.com>
2021-08-12 09:35:21 +0900

    Predicate testing loop for spurious wakeup

--- ps0/caltrain.c
+++ ps0/caltrain.c
@@ -2,6 +2,7 @@
 
 struct station {
   int seats;
+  int count;
   int passengers;
   struct lock lock;
   struct condition arrival;
@@ -10,6 +11,7 @@ struct station {
 
 void station_init(struct station *station) {
   station->seats = 0;
+  station->count = 0;
   station->passengers = 0;
   lock_init(&station->lock);
   cond_init(&station->arrival);
@@ -19,19 +21,25 @@ void station_init(struct station *station) {
 void station_load_train(struct station *station, int count) {
   lock_acquire(&station->lock);
   station->seats = count;
-  for (int i = 0; i < count; i += 1) {
-    cond_signal(&station->arrival, &station->lock);
-  }
-  if (station->passengers > 0 && station->seats > 0) {
+  station->count = count;
+
+  while (station->seats > 0 && station->passengers > 0) {
+    cond_broadcast(&station->arrival, &station->lock);
     cond_wait(&station->departure, &station->lock);
   }
+  station->seats = 0;
+  station->count = 0;
   lock_release(&station->lock);
 }
 
 void station_wait_for_train(struct station *station) {
   lock_acquire(&station->lock);
   station->passengers += 1;
-  cond_wait(&station->arrival, &station->lock);
+
+  while (station->count == 0) {
+    cond_wait(&station->arrival, &station->lock);
+  }
+  station->count -= 1;
   lock_release(&station->lock);
 }
 
