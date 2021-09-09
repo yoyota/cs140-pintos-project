fe1bfcc5dee13e6c545fdeeea762a4b744694082
YongTak Yoo <yongtaky@gmail.com>
2021-09-09 17:34:58 +0900

    Implement simple priority scheduling

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -22,7 +22,7 @@
 
 /* List of processes in THREAD_READY state, that is, processes
    that are ready to run but not actually running. */
-static struct list ready_list;
+static struct list ready_list[PRI_MAX + 1];
 
 /* List of all processes.  Processes are added to this list
    when they are first scheduled and removed when they exit. */
@@ -88,8 +88,11 @@ void thread_init(void)
 	ASSERT(intr_get_level() == INTR_OFF);
 
 	lock_init(&tid_lock);
-	list_init(&ready_list);
 	list_init(&all_list);
+	int i;
+	for (i = 0; i < PRI_MAX + 1; i++) {
+		list_init(&ready_list[i]);
+	}
 
 	/* Set up a thread structure for the running thread. */
 	initial_thread = running_thread();
@@ -237,7 +240,7 @@ void thread_unblock(struct thread *t)
 
 	old_level = intr_disable();
 	ASSERT(t->status == THREAD_BLOCKED);
-	list_push_back(&ready_list, &t->elem);
+	list_push_back(&ready_list[t->priority], &t->elem);
 	t->status = THREAD_READY;
 	intr_set_level(old_level);
 }
@@ -303,7 +306,7 @@ void thread_yield(void)
 
 	old_level = intr_disable();
 	if (cur != idle_thread)
-		list_push_back(&ready_list, &cur->elem);
+		list_push_back(&ready_list[cur->priority], &cur->elem);
 	cur->status = THREAD_READY;
 	schedule();
 	intr_set_level(old_level);
@@ -464,11 +467,13 @@ static void *alloc_frame(struct thread *t, size_t size)
    idle_thread. */
 static struct thread *next_thread_to_run(void)
 {
-	if (list_empty(&ready_list))
-		return idle_thread;
-	else
-		return list_entry(list_pop_front(&ready_list), struct thread,
-				  elem);
+	int i;
+	for (i = PRI_MAX; i >= 0; i--) {
+		if (!list_empty(&ready_list[i]))
+			return list_entry(list_pop_front(&ready_list[i]),
+					  struct thread, elem);
+	}
+	return idle_thread;
 }
 
 /* Completes a thread switch by activating the new thread's page
