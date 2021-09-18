cc006ba8a46305e1966ea98198934afcfe67001e
YongTak Yoo <yongtaky@gmail.com>
2021-09-18 16:01:25 +0900

    Fix arithmetic sequence

--- pintos/src/threads/fixed-point.h
+++ pintos/src/threads/fixed-point.h
@@ -1,5 +1,6 @@
+// q = 14 and f = 2 << 14
 #define Q 14
-#define F 1 << Q
+#define F (1 << Q)
 #define itof(n) (n * F)
 #define ftoi(f) (f / F)
-#define ff_div(x, y) ((((int64_t)x) * F) / y)
+#define ff_div(x, y) ((int64_t)x * F / y)
