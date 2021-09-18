dc473f5299b4664918761e8b3cfc9084ba75f60f
YongTak Yoo <yongtaky@gmail.com>
2021-09-18 16:57:59 +0900

    Use macro

--- pintos/src/threads/fixed-point.h
+++ pintos/src/threads/fixed-point.h
@@ -1,6 +1,6 @@
 // p = 17, q = 14 and f = 1 << 14
-int scaling_factor = (1 << 14);
-#define itof(n) (n * scaling_factor)
-#define ftoi(f) (f / scaling_factor)
-#define ff_div(x, y) ((int64_t)x * scaling_factor / y)
-#define ff_mul(x, y) ((int64_t)x * y / scaling_factor)
+#define SCALING_FACOTR (1 << 14)
+#define itof(n) (n * SCALING_FACOTR)
+#define ftoi(f) (f / SCALING_FACOTR)
+#define ff_div(x, y) ((int64_t)x * SCALING_FACOTR / y)
+#define ff_mul(x, y) ((int64_t)x * y / SCALING_FACOTR)
