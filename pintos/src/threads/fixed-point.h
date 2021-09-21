7623e64dbe1f0cb9414e1a019601c1030b965a68
YongTak Yoo <yongtaky@gmail.com>
2021-09-21 20:04:42 +0900

    Fix typo FACOTR to FACTOR

--- pintos/src/threads/fixed-point.h
+++ pintos/src/threads/fixed-point.h
@@ -1,6 +1,6 @@
 // p = 17, q = 14 and f = 1 << 14
-#define SCALING_FACOTR (1 << 14)
-#define itof(n) (n * SCALING_FACOTR)
-#define ftoi(f) (f / SCALING_FACOTR)
-#define ff_div(x, y) ((int64_t)x * SCALING_FACOTR / y)
-#define ff_mul(x, y) ((int64_t)x * y / SCALING_FACOTR)
+#define SCALING_FACTOR (1 << 14)
+#define itof(n) (n * SCALING_FACTOR)
+#define ftoi(f) (f / SCALING_FACTOR)
+#define ff_div(x, y) ((int64_t)x * SCALING_FACTOR / y)
+#define ff_mul(x, y) ((int64_t)x * y / SCALING_FACTOR)
