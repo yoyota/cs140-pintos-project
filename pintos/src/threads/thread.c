yoyota/cs140-pintos-project

commit 05cef013e73880592e9df64e729a67560853b695

Implement priority calculate all But kernel panick
priority is -1 and kernel panic

pintos/src/threads/thread.c
@@ -190,6 +190,11 @@ void priority_calculate(struct thread *t, void *aux UNUSED)
 	t->priority = PRI_MAX - ftoi(t->recent_cpu / 4) - (t->nice * 2);
 }
 
+void priority_calculate_all()
+{
+	thread_foreach(priority_calculate, NULL);
+}
+
 /* Prints thread statistics. */
 void thread_print_stats(void)
 {

pintos/src/devices/timer.c
@@ -206,6 +206,9 @@ static void timer_interrupt(struct intr_frame *args UNUSED)
 			system_load_avg_calculate();
 			recent_cpu_calculate();
 		}
+		if (ticks % 4 == 0) {
+			priority_calculate_all();
+		}
 	}
 }
 

pintos/src/threads/thread.h
@@ -111,9 +111,10 @@ void thread_init(void);
 void thread_start(void);
 
 void thread_tick(void);
+void system_load_avg_calculate(void);
 void recent_cpu_increase(void);
 void recent_cpu_calculate(void);
-void system_load_avg_calculate(void);
+void priority_calculate_all();
 void thread_print_stats(void);
 
 typedef void thread_func(void *aux);

commit 14e43d848c17f61d16b41dd7d2dd4e090e26f535

Fixes: 05cef01(Implement priority calculate all ...)
Add bounds checking
Fixes #9

pintos/src/threads/thread.c
@@ -187,7 +187,15 @@ void recent_cpu_calculate(void)
 
 void priority_calculate(struct thread *t, void *aux UNUSED)
 {
+	if (t == idle_thread) {
+		return;
+	}
 	t->priority = PRI_MAX - ftoi(t->recent_cpu / 4) - (t->nice * 2);
+	if (t->priority < PRI_MIN) {
+		t->priority = PRI_MIN;
+	} else if (t->priority > PRI_MAX) {
+		t->priority = PRI_MAX;
+	}
 }
 
 void priority_calculate_all()