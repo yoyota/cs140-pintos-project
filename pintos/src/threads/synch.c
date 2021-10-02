948190fcbf1915f45620154f303d8d8627012145
YongTak Yoo <yongtaky@gmail.com>
2021-10-02 18:20:26 +0900

    Immediately schedule if current has lower priority
    Fixes #10

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -106,10 +106,10 @@ void sema_up(struct semaphore *sema)
 	ASSERT(sema != NULL);
 
 	old_level = intr_disable();
+	sema->value++;
 	if (!list_empty(&sema->waiters))
 		thread_unblock(list_entry(list_pop_front(&sema->waiters),
 					  struct thread, elem));
-	sema->value++;
 	intr_set_level(old_level);
 }
 
